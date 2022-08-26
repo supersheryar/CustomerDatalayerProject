39b2c17e0d999485ce86ec49d01c8a770c1b95bf
Sheryar <supersheryarshah@gmail.com>
2022-08-26 04:27:18 +0300

    feat: added BaseRepository and did refractoring for net472

---
+++ src/CustomerDatalayer/Repositories/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerDatalayer.Repositories
+{
+    public class BaseRepository
+    {
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection("Server=.\\SQLEXPRESS;Database=CustomerDB_Kalenishin;Trusted_Connection=True;");
+        }
+    }
+}
--- src/CustomerDatalayer/Repositories/CustomerRepository.cs
+++ src/CustomerDatalayer/Repositories/CustomerRepository.cs
@@ -10,128 +10,143 @@ using System.Threading.Tasks;
 
 namespace CustomerDatalayer.Repositories
 {
-    public class CustomerRepository : IRepository<Customers>
+    public class CustomerRepository : BaseRepository, IRepository<Customers>
     {
         public void Create(Customers entity)
         {
-            using var connection = new SqlConnection("Server=.\\WIN-DP2TDF27BOE\\SQLEXPRESS;Database=CustomerDB_Kalenishin;Trusted_Connection=True;");
-            connection.Open();
-            var command = new SqlCommand(
-                "INSERT INTO Customers(FirstName, LastName, PhoneNumber, Email, TotalPurchasesAmount)" +
-                "VALUES (@FirstName, @LastName, @PhoneNumber, @Email, @TotalPurchasesAmount)",
-                connection);
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                var command = new SqlCommand(
+                    "INSERT INTO Customers(FirstName, LastName, PhoneNumber, Email, TotalPurchasesAmount)" +
+                    "VALUES (@FirstName, @LastName, @PhoneNumber, @Email, @TotalPurchasesAmount)",
+                    connection);
 
-            var customerFirstNameParam = new SqlParameter("@FirstName", System.Data.SqlDbType.NVarChar, 50)
-            {
-                Value = entity.FirstName
-            };
-            var customerLastNameParam = new SqlParameter("@LastName", System.Data.SqlDbType.NVarChar, 50)
-            {
-                Value = entity.LastName
-            };
-            var customerPhoneNumberParam = new SqlParameter("@PhoneNumber", System.Data.SqlDbType.NVarChar, 15)
-            {
-                Value = entity.PhoneNumber
-            };
-            var customerEmailParam = new SqlParameter("@Email", System.Data.SqlDbType.NVarChar, 100)
-            {
-                Value = entity.Email
-            };
-            var customerTotalPurchasesAmountParam = new SqlParameter("@TotalPurchasesAmount", System.Data.SqlDbType.Decimal)
-            {
-                Value = entity.TotalPurchasesAmount
-            };
-            command.Parameters.Add(customerFirstNameParam);
-            command.Parameters.Add(customerLastNameParam);
-            command.Parameters.Add(customerPhoneNumberParam);
-            command.Parameters.Add(customerEmailParam);
-            command.Parameters.Add(customerTotalPurchasesAmountParam);
-            command.ExecuteNonQuery();
+                var customerFirstNameParam = new SqlParameter("@FirstName", System.Data.SqlDbType.NVarChar, 50)
+                {
+                    Value = entity.FirstName
+                };
+                var customerLastNameParam = new SqlParameter("@LastName", System.Data.SqlDbType.NVarChar, 50)
+                {
+                    Value = entity.LastName
+                };
+                var customerPhoneNumberParam = new SqlParameter("@PhoneNumber", System.Data.SqlDbType.NVarChar, 15)
+                {
+                    Value = entity.PhoneNumber
+                };
+                var customerEmailParam = new SqlParameter("@Email", System.Data.SqlDbType.NVarChar, 100)
+                {
+                    Value = entity.Email
+                };
+                var customerTotalPurchasesAmountParam = new SqlParameter("@TotalPurchasesAmount", System.Data.SqlDbType.Decimal)
+                {
+                    Value = entity.TotalPurchasesAmount
+                };
+                command.Parameters.Add(customerFirstNameParam);
+                command.Parameters.Add(customerLastNameParam);
+                command.Parameters.Add(customerPhoneNumberParam);
+                command.Parameters.Add(customerEmailParam);
+                command.Parameters.Add(customerTotalPurchasesAmountParam);
+                command.ExecuteNonQuery();
+            }
         }
 
 
         public Customers Read(int entityID)
         {
-            using var connection = new SqlConnection("Server=.\\SQLEXPRESS;Database=CustomerDB_Kalenishin;Trusted_Connection=True;");
-            connection.Open();
-            var command = new SqlCommand("SELECT * FROM [Customers] WHERE CustomerId = @CustomerId", connection);
-            var customerIDParam = new SqlParameter("@CustomerId", SqlDbType.Int)
+            using (var connection = GetConnection())
             {
-                Value = entityID
-            };
-            command.Parameters.Add(customerIDParam);
-            using var reader = command.ExecuteReader();
-            if (reader.Read())
-            {
-                return new Customers
+                connection.Open();
+                var command = new SqlCommand("SELECT * FROM [Customers] WHERE CustomerId = @CustomerId", connection);
+                var customerIDParam = new SqlParameter("@CustomerId", SqlDbType.Int)
                 {
-                    FirstName = reader["FirstName"].ToString(),
-                    LastName = reader["LastName"].ToString(),
-                    PhoneNumber = reader["PhoneNumber"].ToString(),
-                    Email = reader["Email"].ToString(),
-                    TotalPurchasesAmount = Convert.ToDecimal(reader["TotalPurchasesAmount"])
+                    Value = entityID
                 };
+                command.Parameters.Add(customerIDParam);
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Customers
+                        {
+                            FirstName = reader["FirstName"].ToString(),
+                            LastName = reader["LastName"].ToString(),
+                            PhoneNumber = reader["PhoneNumber"].ToString(),
+                            Email = reader["Email"].ToString(),
+                            TotalPurchasesAmount = Convert.ToDecimal(reader["TotalPurchasesAmount"])
+                        };
+                    }
+                }
+
+                return new Customers();
             }
-            return new Customers();
         }
 
 
         public void Update(Customers entity)
         {
-            using var connection = new SqlConnection("Server=.\\SQLEXPRESS;Database=CustomerDB_Kalenishin;Trusted_Connection=True;");
-            connection.Open();
-            var command = new SqlCommand("UPDATE [Customers] SET FirstName = @FirstName WHERE CustomerId = @CustomerId", connection);
-            var customerIDParam = new SqlParameter("@CustomerId", System.Data.SqlDbType.Int)
+            using (var connection = GetConnection())
             {
-                Value = entity.CustomerId
-            };
-            var customerFirstNameParam = new SqlParameter("@FirstName", System.Data.SqlDbType.NVarChar, 50)
-            {
-                Value = entity.FirstName
-            };
-            command.Parameters.Add(customerIDParam);
-            command.Parameters.Add(customerFirstNameParam);
-            command.ExecuteNonQuery();
+                connection.Open();
+                var command = new SqlCommand("UPDATE [Customers] SET FirstName = @FirstName WHERE CustomerId = @CustomerId", connection);
+                var customerIDParam = new SqlParameter("@CustomerId", System.Data.SqlDbType.Int)
+                {
+                    Value = entity.CustomerId
+                };
+                var customerFirstNameParam = new SqlParameter("@FirstName", System.Data.SqlDbType.NVarChar, 50)
+                {
+                    Value = entity.FirstName
+                };
+                command.Parameters.Add(customerIDParam);
+                command.Parameters.Add(customerFirstNameParam);
+                command.ExecuteNonQuery();
+            }
         }
 
 
         public void Delete(int entityID)
         {
-            using var connection = new SqlConnection("Server=.\\SQLEXPRESS;Database=CustomerDB_Kalenishin;Trusted_Connection=True;");
-            connection.Open();
-            var command = new SqlCommand("DELETE FROM [Customers] WHERE CustomerId = @CustomerId", connection);
-            var customerIDParam = new SqlParameter("@CustomerId", System.Data.SqlDbType.Int)
+            using (var connection = GetConnection())
             {
-                Value = entityID
-            };
-            command.Parameters.Add(customerIDParam);
-            command.ExecuteNonQuery();
+                connection.Open();
+                var command = new SqlCommand("DELETE FROM [Customers] WHERE CustomerId = @CustomerId", connection);
+                var customerIDParam = new SqlParameter("@CustomerId", System.Data.SqlDbType.Int)
+                {
+                    Value = entityID
+                };
+                command.Parameters.Add(customerIDParam);
+                command.ExecuteNonQuery();
+            }
         }
 
 
         public int GetCustomerId()
         {
-            using var connection = new SqlConnection("Server=.\\SQLEXPRESS;Database=CustomerDB_Kalenishin;Trusted_Connection=True;");
-            connection.Open();
-            var command = new SqlCommand("SELECT CustomerId FROM Customers", connection);
-            var reader = command.ExecuteReader();
-            if (reader.Read())
+            using (var connection = GetConnection())
             {
-                return Convert.ToInt32(reader["CustomerId"]);
+                connection.Open();
+                var command = new SqlCommand("SELECT CustomerId FROM Customers", connection);
+                var reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    return Convert.ToInt32(reader["CustomerId"]);
+                }
+                return 0;
             }
-            return 0;
         }
 
 
         public void DeleteAll()
         {
-            using var connection = new SqlConnection("Server=.\\SQLEXPRESS;Database=CustomerDB_Kalenishin;Trusted_Connection=True;");
-            connection.Open();
+            using (var connection = GetConnection())
+            {
+                connection.Open();
 
-            var command = new SqlCommand(
-                "DELETE FROM Customers",
-                connection);
-            command.ExecuteNonQuery();
+                var command = new SqlCommand(
+                    "DELETE FROM Customers",
+                    connection);
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
